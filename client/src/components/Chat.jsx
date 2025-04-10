207428feb44d8d269501082f8df26ef28208beac
Althaf A <althafmuhammed484@gmail.com>
2025-04-10 18:57:07 +0530

    done basic server set up and Join component

---
+++ client/src/components/Chat.jsx
@@ -0,0 +1,9 @@
+import react from "react";
+
+const Chat = () =>{
+    return (
+        <h1>Chat</h1>
+    )
+}
+
+export default Chat;
---
+++ client/src/components/Join.jsx
@@ -0,0 +1,9 @@
+import react from "react";
+
+const Join = () =>{
+    return (
+        <h1>Join</h1>
+    )
+}
+
+export default Join;
